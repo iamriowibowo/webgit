d343663a763ee974873eea6cdfa1eddfaabc45e6
NIMDA\Admin <satrio.wibowo@jasnita.co.id>
2023-07-03 14:31:51 +0700

    second route register

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Route::get('/register', function () {
     return "register page";
 });
+
+
+Route::get('/register2', function () {
+    return "register page 2";
+});
