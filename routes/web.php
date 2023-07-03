e4b7b510a36aede1e5648d20b991458bca348597
NIMDA\Admin <satrio.wibowo@jasnita.co.id>
2023-07-03 16:54:25 +0700

    git checkout route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Process\Process;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +18,57 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return "login page";
+Route::get('/git/branches', function () {
+    // Create a new process to execute the git branch command
+    $process = new Process(['C:\laragon\bin\git\bin\git.exe', 'branch']);
+
+    // Run the process and capture the output
+    $process->run();
+
+    // Get the output as a string
+    $output = $process->getOutput();
+
+    // Process the output to extract the branch names
+    $branches = explode(PHP_EOL, trim($output));
+
+    // Variable to store branch name
+    $arrayBranches = explode("\n  ", str_replace('\n', "\n", $branches[0]));
+
+    // Print the list of branches
+    return $arrayBranches;
 });
 
-Route::get('/register', function () {
-    return "register page";
-});
+Route::get('/git/branches/checkout', function () {
+    $coBr = 'feature/login';
 
+    // Create a new process to execute the git checkout command
+    $checkoutProcess = new Process(['C:\laragon\bin\git\bin\git.exe', 'checkout', $coBr]);
 
-Route::get('/register2', function () {
-    return "register page 2";
+    // Run the checkout process
+    $checkoutProcess->run();
+
+    // Check if the checkout was successful
+    if ($checkoutProcess->isSuccessful()) {
+        echo 'Branch checked out successfully.';
+    } else {
+        echo 'Failed to checkout branch.';
+    }
+
+    // Create a new process to execute the git branch command
+    $process = new Process(['C:\laragon\bin\git\bin\git.exe', 'branch']);
+
+    // Run the process and capture the output
+    $process->run();
+
+    // Get the output as a string
+    $output = $process->getOutput();
+
+    // Process the output to extract the branch names
+    $branches = explode(PHP_EOL, trim($output));
+
+    // Variable to store branch name
+    $arrayBranches = explode("\n  ", str_replace("\n", "\n", $branches[0]));
+
+    // Print the list of branches
+    return $arrayBranches;
 });
